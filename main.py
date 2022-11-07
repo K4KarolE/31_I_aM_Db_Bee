6b7318113b7a224d5d2bc8d19ddbe04691ff0230
K4KarolE <karoly.egyed@protonmail.com>
2022-11-07 13:40:04 +0000

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ test 2
 - should be able to select automatically the callable script/scenario: Movie - Series - TV
 - link from clipboard
 - how to pass the link from main to the called script?
-- buttons: requirements, help, excel temp downloadable?, contact(email)
+- buttons: get link, start, requirements, help, excel temp downloadable?, contact(email)
 - field where able to select the excel file and location?
     + file name valuation, if not using the excel sheet provided -> warning message?
     + able to save as default?
@@ -12,7 +12,7 @@ test 2
 - error messages pop-up?
 - bee waving animation? hand and eyebrow? (looping images?)
 
-- looking for hungarian title switchable?
+- looking for hungarian title? switchable button? clever how to pass to the called script?
 
 Requirements:
 - Python
