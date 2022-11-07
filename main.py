274b33e9a0848af936979f06a8f91bcbe58ef846
K4KarolE <karoly.egyed@protonmail.com>
2022-11-07 09:08:30 +0000

    prep

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+
+- should be able to select automatically the callable script/scenario: Movie - Series - TV
+- link from clipboard
+- how to pass the link from main to the called script?
+- buttons: requirements, help, excel temp downloadable?, contact(email)
+- field where able to select the excel file and location?
+    + file name valuation, if not using the excel sheet provided -> warning message?
+    + able to save as default?
+- unique window icon? (top left)
+- able to amend the frame?
+- error messages pop-up?
+- bee waving animation? hand and eyebrow? (looping images?)
+
+- looking for hungarian title switchable?
+
+Requirements:
+- Python
+- Selenium Webdriver
+- pop-up for first time users? how to tell first time user?
+
+'''
