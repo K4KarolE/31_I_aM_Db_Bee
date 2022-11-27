fb5ce431aef360e4f592d410add7d8c2cfd4ef99
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2022-11-27 12:13:15 +0000

    1st steps

--- main.py
+++ main.py
@@ -20,3 +20,26 @@ Requirements:
 - pop-up for first time users? how to tell first time user?
 
 '''
+
+from bs4 import BeautifulSoup
+import requests
+
+# html_text = requests.get('https://www.imdb.com/title/tt0104952/?ref_=nv_sr_srsg_0').text  #Movie - 1 director
+html_text = requests.get('https://www.imdb.com/title/tt0120855/?ref_=nv_sr_srsg_0').text  #Movie - 2 Directors
+# html_text = requests.get('https://www.imdb.com/title/tt0944947/?ref_=nv_sr_srsg_0').text  #TV Show
+# html_text = requests.get('https://www.imdb.com/title/tt15318872/?ref_=adv_li_tt').text  #TV Special
+soup = BeautifulSoup(html_text, 'lxml')
+
+title = soup.find(class_='sc-b73cd867-0 eKrKux').text
+year = soup.find(class_='sc-8c396aa2-2 itZqyK').text
+directors = soup.find(class_='sc-fa02f843-0 fjLeDR')
+director_first = directors.find('li', class_='ipc-inline-list__item').text
+
+
+print()
+print(f"{title} - {year}\n")
+print(director_first + '\n')
+
+
+
+
