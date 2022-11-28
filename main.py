714b7031802aaa1006b324f42a8dec6acda8b63f
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2022-11-28 12:04:12 +0000

    mixing

--- main.py
+++ main.py
@@ -25,57 +25,58 @@ from bs4 import BeautifulSoup
 import requests
 
 # html_text = requests.get('https://www.imdb.com/title/tt0104952/?ref_=nv_sr_srsg_0').text  #Movie - 1 director
-html_text = requests.get('https://www.imdb.com/title/tt0120855/?ref_=nv_sr_srsg_0').text  #Movie - 2 Directors
+# html_text = requests.get('https://www.imdb.com/title/tt0120855/?ref_=nv_sr_srsg_0').text  #Movie - 2 Directors
 # html_text = requests.get('https://www.imdb.com/title/tt0944947/?ref_=nv_sr_srsg_0').text  #TV Show
-# html_text = requests.get('https://www.imdb.com/title/tt15318872/?ref_=adv_li_tt').text  #TV Special
+html_text = requests.get('https://www.imdb.com/title/tt15318872/?ref_=adv_li_tt').text  #TV Special
 soup = BeautifulSoup(html_text, 'lxml')
 
 title = soup.find(class_='sc-b73cd867-0 eKrKux').text
 year = soup.find(class_='sc-8c396aa2-2 itZqyK').text
 
-# Director(s) - Actor(s)
-director_actor = soup.find(class_='sc-fa02f843-0 fjLeDR')
-section = director_actor.find_all('div')
+## Directors - Actors
+crew_content = soup.find(class_='sc-fa02f843-0 fjLeDR')
+crew_tag = crew_content.find_all('div')
 
 i_counter = 0
 directors = []
 actors = []
-for i in section:       # in 3 divesion we looking for the 1st(director) and 3rd(actor)
-    i_counter += 1      # names, with no extra info like: (directed by), (voice)
-    for k in i:
-        for t in k:
-            if i_counter == 1:
-                directors = directors + [t.a.text]
-            if i_counter == 3:
-                actors = actors + [t.a.text]
 
+if soup.find(class_='sc-89e7233a-1 dWZpw episode-guide-text'):  # TV Show - Episode guide section - only in shows/series
+    for i in crew_tag:       
+        i_counter += 1      
+        for k in i:
+            for t in k:
+                if i_counter == 2:
+                    actors = actors + [t.a.text]
+
+else:                                       # Movie / TV Special
+    for i in crew_tag:       
+        i_counter += 1      
+        for k in i:
+            for t in k:
+                if i_counter == 1:
+                    directors = directors + [t.a.text]
+                if i_counter == 3:
+                    actors = actors + [t.a.text]
+
+## Genre
+genre_content = soup.find(class_='ipc-chip-list--baseAlt ipc-chip-list sc-16ede01-5 ggbGKe')
+genre_tag = genre_content.find_all('a')
+genres = []
+for i in genre_tag:
+    genres = genres + [i.text]
+
+
+
+
+
+print()
+print(genres)
+print()
 print(directors)
 print()
 print(actors)
-
-
-# directors = director_ul.find_all('li')
-# directors_list = []
-# for i in directors:
-#     directors_list = directors_list + [i.text]
-
-# actor_ul = director_actor.find('div', class_='ipc-metadata-list-item__content-container')
-# actors = actor_ul.find_all('li')
-# actors_list = []
-# for i in actors:
-#     actors_list = actors_list + [i.text]
-
-
-# print()
-# print(f"{title} - {year}\n")
-# print('Director(s):')
-# for i in directors_list:
-#     print(f"{i} ")
-
-# print()
-
-# for i in actors_list:
-#     print(f"{i} ")
+print()
 
 
 
