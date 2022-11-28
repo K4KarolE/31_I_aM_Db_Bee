92756bd094468645f98d69e2e529851641ab0627
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2022-11-28 08:16:48 +0000

    director/actor lists

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ test 2
 
 Requirements:
 - Python
-- Selenium Webdriver
+- bs4
 - pop-up for first time users? how to tell first time user?
 
 '''
@@ -32,21 +32,50 @@ soup = BeautifulSoup(html_text, 'lxml')
 
 title = soup.find(class_='sc-b73cd867-0 eKrKux').text
 year = soup.find(class_='sc-8c396aa2-2 itZqyK').text
-directors_search = soup.find(class_='sc-fa02f843-0 fjLeDR')
-director_ul = directors_search.find('ul', class_='ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content baseAlt')
-directors = director_ul.find_all('li')
 
-directors_list = []
-for i in directors:
-    directors_list = directors_list + [i.text]
+# Director(s) - Actor(s)
+director_actor = soup.find(class_='sc-fa02f843-0 fjLeDR')
+section = director_actor.find_all('div')
 
+i_counter = 0
+directors = []
+actors = []
+for i in section:       # in 3 divesion we looking for the 1st(director) and 3rd(actor)
+    i_counter += 1      # names, with no extra info like: (directed by), (voice)
+    for k in i:
+        for t in k:
+            if i_counter == 1:
+                directors = directors + [t.a.text]
+            if i_counter == 3:
+                actors = actors + [t.a.text]
 
-
+print(directors)
 print()
-print(f"{title} - {year}\n")
-print('Director(s):')
-for i in directors_list:
-    print(f"{i} ")
+print(actors)
+
+
+# directors = director_ul.find_all('li')
+# directors_list = []
+# for i in directors:
+#     directors_list = directors_list + [i.text]
+
+# actor_ul = director_actor.find('div', class_='ipc-metadata-list-item__content-container')
+# actors = actor_ul.find_all('li')
+# actors_list = []
+# for i in actors:
+#     actors_list = actors_list + [i.text]
+
+
+# print()
+# print(f"{title} - {year}\n")
+# print('Director(s):')
+# for i in directors_list:
+#     print(f"{i} ")
+
+# print()
+
+# for i in actors_list:
+#     print(f"{i} ")
 
 
 
