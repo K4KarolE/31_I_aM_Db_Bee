6a9f871b3ba4959df7486254f54036630084751d
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2023-01-24 10:55:14 +0000

    skin update fix, BG update, format update

--- main.py
+++ main.py
@@ -35,58 +35,7 @@ backgound_image_label.place(x = -2, y = 0)
 window.iconbitmap(f"./skins/{skin_selected}/icon.ico")        # window icon
 
 
-## SKINS - ROLL DOWN MENU
-def change_skin(__):
-    settings_data['skin_selected'] = skins_roll_down_clicked.get()  # updating & saving the "skin_selected" value in settings_db.json with every click/skin change
-    settings.save_settings(settings_data)
-    skin_selected = skins_roll_down_clicked.get()
-    # LIST OF WIDGETS TO UPDATE
-    #TEXT
-    window.title(settings_data['skins'][skin_selected]['window_title'])
-    #IMAGES
-    backgound_image.configure(file = f"./skins/{skin_selected}/BG.png")
-    window.iconbitmap(f"./skins/{skin_selected}/icon.ico")
-    #COLORS
-    background_color = settings_data['skins'][skin_selected]['background_color']
-    font_color = settings_data['skins'][skin_selected]['font_color']
-    print(font_color)
-    skins_roll_down.configure(foreground=font_color, background=background_color, activebackground=background_color, highlightbackground=background_color)
-    skins_roll_down['menu'].configure(background=background_color, activebackground=background_color, activeforeground = "white")
-
-    title_search_roll_down.configure(foreground=font_color, background=background_color, activebackground=background_color, highlightbackground=background_color)
-    title_search_roll_down['menu'].configure(foreground=font_color, background=background_color, activeforeground = font_color, activebackground=background_color)
-
-    target_sheet_field.configure(foreground=font_color, background=background_color)
-    target_sheet_field_title.configure(foreground=font_color, background=background_color)
-    target_sheet_button.configure(foreground=font_color, background=background_color)
-
-    movies_db_sheet_field.configure(foreground=font_color, background=background_color)
-    movies_db_sheet_field_title.configure(foreground=font_color, background=background_color)
-    movies_db_sheet_button.configure(foreground=font_color, background=background_color)
-
-    chrome_driver_field.configure(foreground=font_color, background=background_color)
-    chrome_driver_field_title.configure(foreground=font_color, background=background_color)
-    chrome_driver_button.configure(foreground=font_color, background=background_color)
-
-    poster_roll_down.config(foreground=font_color, background=background_color, activebackground=background_color, highlightbackground=background_color)
-    poster_roll_down["menu"].config(foreground=font_color, background=background_color, activeforeground = "white", activebackground=background_color)
-
-    for item in checkbox.values():
-        item[1].configure(foreground=font_color, background=background_color, activeforeground = font_color, activebackground=background_color, highlightbackground=background_color)
-
-    button_save_and_start.configure(foreground=font_color, background=background_color)
-
-skins_options = []
-for item in settings_data['skins'].keys():        # creating a list of the SKINS from settings_db.json / skins
-    skins_options = skins_options + [item]
-
-skins_roll_down_clicked = StringVar()
-skins_roll_down_clicked.set("Skins")    
-skins_roll_down = OptionMenu( window, skins_roll_down_clicked, *skins_options, command=change_skin)     
-skins_roll_down.configure(foreground=font_color, background=background_color, activeforeground = font_color, activebackground=background_color, highlightbackground=background_color)
-skins_roll_down['menu'].configure(foreground=font_color, background=background_color, activebackground=background_color)
-
-# CHECKBOXES
+## CHECKBOXES
 checkbox = {
     'clipboard': ['imdb_link_in_clipboard', 'clipboard_button', 'IMDb link in clipboard' ],     # [0/1, button, text]
     'poster': ['poster_open_in_new_tab', 'poster_open_in_new_tab_button', 'Poster in a new tab' ],
@@ -111,7 +60,71 @@ for item in checkbox.values():
         )
 checkbox['no_picture'][1].config(font = (font_style, 9))    # make the /No pictures in target sheet/ checkbox text smaller
 
-# TITLE SEARCH - BUTTON
+
+## ROLL DOWN MENUS
+# SKIN - ROLL DOWN MENU
+# highlightbackground - color around the button
+# activebackground - color when mouse over or clicked
+def change_skin(__):
+    settings_data['skin_selected'] = skins_roll_down_clicked.get()  # updating & saving the "skin_selected" value in settings_db.json with every click/skin change
+    settings.save_settings(settings_data)
+    skin_selected = skins_roll_down_clicked.get()
+    # if skin_selected == "darth":                      # for future buttons movement: IMDB, HELP
+    #     skins_roll_down.place(x=50, y=200)
+    # if skin_selected == "default":
+    #     skins_roll_down.place(x=7, y=200)
+
+    # LIST OF WIDGETS TO UPDATE
+    #TEXT
+    window.title(settings_data['skins'][skin_selected]['window_title'])
+    #IMAGES
+    backgound_image.configure(file = f"./skins/{skin_selected}/BG.png")
+    window.iconbitmap(f"./skins/{skin_selected}/icon.ico")
+    ##COLORS
+    background_color = settings_data['skins'][skin_selected]['background_color']
+    font_color = settings_data['skins'][skin_selected]['font_color']
+    
+    # CHECKBOXES
+    for item in checkbox.values():
+        item[1].configure(foreground=font_color, background=background_color, activeforeground = font_color, activebackground=background_color, highlightbackground=background_color)
+    
+    # ROLL DOWN MENUS
+    skins_roll_down.configure(foreground=font_color, background=background_color, activeforeground=font_color, activebackground=background_color, highlightbackground=background_color)
+    skins_roll_down['menu'].configure(foreground=font_color, background=background_color, activebackground=background_color, activeforeground = "white")
+
+    title_search_roll_down.configure(foreground=font_color, background=background_color, activeforeground=font_color, activebackground=background_color, highlightbackground=background_color)
+    title_search_roll_down['menu'].configure(foreground=font_color, background=background_color, activeforeground = "white", activebackground=background_color)
+
+    poster_roll_down.config(foreground=font_color, background=background_color, activeforeground=font_color, activebackground=background_color, highlightbackground=background_color)
+    poster_roll_down["menu"].config(foreground=font_color, background=background_color, activeforeground = "white", activebackground=background_color)
+
+    # PATH - FIELDS + SEARCHBOXES
+    target_sheet_field.configure(foreground=font_color, background=background_color)
+    target_sheet_field_title.configure(foreground=font_color, background=background_color)
+    target_sheet_button.configure(foreground=font_color, background=background_color)
+
+    movies_db_sheet_field.configure(foreground=font_color, background=background_color)
+    movies_db_sheet_field_title.configure(foreground=font_color, background=background_color)
+    movies_db_sheet_button.configure(foreground=font_color, background=background_color)
+
+    chrome_driver_field.configure(foreground=font_color, background=background_color)
+    chrome_driver_field_title.configure(foreground=font_color, background=background_color)
+    chrome_driver_button.configure(foreground=font_color, background=background_color)
+
+    # SAVE AND START BUTTON
+    button_save_and_start.configure(foreground=font_color, background=background_color)
+
+skins_options = []
+for item in settings_data['skins'].keys():        # creating a list of the SKINS from settings_db.json / skins
+    skins_options = skins_options + [item]
+
+skins_roll_down_clicked = StringVar()
+skins_roll_down_clicked.set("Skins")    
+skins_roll_down = OptionMenu( window, skins_roll_down_clicked, *skins_options, command=change_skin)     
+skins_roll_down.configure(foreground=font_color, background=background_color, activeforeground = font_color, activebackground=background_color, highlightbackground=background_color)
+skins_roll_down['menu'].configure(foreground=font_color, background=background_color, activebackground=background_color)
+
+# TITLE SEARCH - ROLL DOWN MENU
 title_search_options = []
 for item in settings_data['title_search_links'].keys():
     title_search_options = title_search_options + [item]    # creating a list of the "title_search_links" dictonary`s keys (Hungarian / Czech /..) from settings_db.json
@@ -121,8 +134,18 @@ title_search_roll_down_clicked.set(settings_data['title_search_link_selected']) 
 title_search_roll_down = OptionMenu( window, title_search_roll_down_clicked, *title_search_options )
 title_search_roll_down.configure(foreground=font_color, background=background_color, activeforeground = font_color, activebackground=background_color, highlightbackground=background_color)
 title_search_roll_down['menu'].configure(foreground=font_color, background=background_color, activebackground=background_color)
-# highlightbackground - color around the button
-# activebackground - color when mouse over or clicked
+
+# POSTER SIZE - ROLL DOWN MENU
+poster_size_options = []
+for item in settings_data['poster_size_options'].keys():        # creating a list of the POSTER SIZE OPTIONS holded in settings_db.json / poster_size_options
+    poster_size_options = poster_size_options + [item]
+
+poster_roll_down_clicked = StringVar()
+poster_roll_down_clicked.set(settings_data['poster_size'])       # # set to the latest saved value
+poster_roll_down = OptionMenu( window, poster_roll_down_clicked, *poster_size_options)
+poster_roll_down.config(foreground=font_color, background=background_color, activeforeground = font_color, activebackground=background_color, highlightbackground=background_color)
+poster_roll_down["menu"].config(foreground=font_color, background=background_color, activebackground=background_color)
+
 
 ## PATH - FIELDS + SEARCHBOXES
 # TARGET SHEET - FIELD + SEARCHBOX
@@ -176,18 +199,7 @@ def browseSheet_3():
     chrome_driver_field.insert(END,filename)     # adding the path and the name of the selected file
 chrome_driver_button = Button(window, text = ">>", command = browseSheet_3, foreground=font_color, background=background_color)
 
-## POSTER SIZE - ROLL DOWN MENU
-poster_size_options = []
-for item in settings_data['poster_size_options'].keys():        # creating a list of the POSTER SIZE OPTIONS holded in settings_db.json / poster_size_options
-    poster_size_options = poster_size_options + [item]
 
-
-
-poster_roll_down_clicked = StringVar()
-poster_roll_down_clicked.set(settings_data['poster_size'])       # # set to the latest saved value
-poster_roll_down = OptionMenu( window, poster_roll_down_clicked, *poster_size_options)
-poster_roll_down.config(foreground=font_color, background=background_color, activeforeground = font_color, activebackground=background_color, highlightbackground=background_color)
-poster_roll_down["menu"].config(foreground=font_color, background=background_color, activebackground=background_color)
 
 ### SAVE SETTINGS, START THE ENGINE
 def save_and_start():
@@ -223,33 +235,32 @@ def save_and_start():
 
     settings.save_settings(settings_data)
 
+    
     ### START ###
     ## MANDATORY FIELDS CHECK   
-    error_popup_window_title = [
-        'Got stuck in honey',
-        'Danger Will Robinson, danger!'
-        ]
+    error_popup_window_title = settings_data['skins'][skin_selected]['error_window_title']  # after skin switch the window title will be updated once the prog. restarted
     # CHROME DRIVER
     if chrome_driver_field.get("1.0", "end-1c") == '':
-        tkinter.messagebox.showinfo(error_popup_window_title[1], f"#{chrome_driver_text}# needs to be added")   # error pop-up message
+        tkinter.messagebox.showinfo(error_popup_window_title, f"#{chrome_driver_text}# needs to be added")   # error pop-up message
         return      # stops the function without exiting from the main script
     # TARGET SHEET
     if target_sheet_field.get("1.0", "end-1c") == '':
-        tkinter.messagebox.showinfo(error_popup_window_title[1], f"#{target_sheet_text}# needs to be added")   # error pop-up message
+        tkinter.messagebox.showinfo(error_popup_window_title, f"#{target_sheet_text}# needs to be added")   # error pop-up message
         return
     # NO PICTURES IN TARGET SHEET - CHECKBOX
     if checkbox['no_picture'][0].get() == 0:
-        tkinter.messagebox.showinfo(error_popup_window_title[1], f"#{checkbox['no_picture'][2]}# checkbox is not selected")   # error pop-up message
+        tkinter.messagebox.showinfo(error_popup_window_title, f"#{checkbox['no_picture'][2]}# checkbox is not selected")   # error pop-up message
         return
     # IMDb LINK IN CLIPBOARD - CHECKBOX
     if checkbox['clipboard'][0].get() == 0:
-        tkinter.messagebox.showinfo(error_popup_window_title[1], f"#{checkbox['clipboard'][2]}# checkbox is not selected\n\nClick OK once the link is copied")
+        tkinter.messagebox.showinfo(error_popup_window_title, f"#{checkbox['clipboard'][2]}# checkbox is not selected\n\nClick OK once the link is copied")
         # error pop-up message, more relevant for the first time users
     engine.start_engine()   # will start data collection / save to excel sheet / if selected: open poster and native title search in new tabs, open movie DB sheet...
     
 button_save_and_start = Button(window, text = "Save & Start", command = save_and_start, font = (font_style, 15), foreground=font_color, background=background_color)        
 # no () in save_and_start() otherwise will execute it automatically before clicking the button
 # binding multiple commands to the same button: command = lambda: [save_settings(), engine.start_engine()]
+
 
 ### DISPLAY WIDGETS
 def display_widgets():
@@ -261,6 +272,7 @@ def display_widgets():
     display_y_BASE = 130
     display_y_GAP = 30
 
+    
     def y_location(gap_by_number):
         display_y = display_y_BASE + display_y_GAP * gap_by_number
         return display_y
@@ -304,6 +316,7 @@ def display_widgets():
     button_save_and_start.place(x=display_x+30, y=y_location(12)+10)
 
     # SKINS ROLL DOWN BUTTON
+    
     skins_roll_down.place(x=7, y=y_location(11))
 
 display_widgets()
