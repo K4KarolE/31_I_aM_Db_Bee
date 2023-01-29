4c7d0818e466196779812fa74f0f61fb8fd04aa0
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2023-01-29 08:19:12 +0000

    adding test

--- functions/own_language_title.py
+++ functions/own_language_title.py
@@ -8,3 +8,7 @@ def search(titleRead, yearRead):
         selected_default_link = settings_data['title_search_links'][settings_data['title_search_link_selected']]    # settings_db.json / 'title_search_links' dictionary looking for the 
         link = selected_default_link + ' '.join([titleRead, yearRead])                                              # saved value(link) of the key: Hungarian or Czech..
         webbrowser.open(link)
+
+## TEST
+# use /import settings/ instead of /from functions import settings/
+# search("Forrest Gump","1994")
